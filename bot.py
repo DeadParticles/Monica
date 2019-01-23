54858f1f8cd2fa1f79bf5a7222099066436d9ca2
DeadParticles <zafterious@gmail.com>
2019-01-23 16:21:54 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,1 +1,9 @@
+import discord
+from discord.ext import commands
+from discord.ext.commands import bot
+import asyncio
+import random
+import requests
+import os
+
 client.run(str(os.environ.get('BOT_TOKEN')))
